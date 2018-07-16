2fcf46697b77a94e5a737e51dd51235a9b8a1020
almaz <almaz_73@mail.ru>
2018-07-16 17:28:41 +0300

    progress(Maybe)

--- pages/Task2.vue
+++ pages/Task2.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>9999999</div>
+    <div>Blackquadro</div>
+  <div class="New-area">
+
+  </div>
+    <div class="blackrectangle">
+
+    </div>
 </template>
 
 <script>
@@ -9,5 +15,21 @@
 </script>
 
 <style scoped>
-
+  h2{
+    top: 20px;
+    text-align: center;
+  }
+  .New-area{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .blackrectangle{
+    background: black;
+    width: 100px;
+    height: 100px;
+  }
 </style>
