2efc385ea4e2e0d4e19da4b1bf4676905e5daa2e
almaz <almaz_73@mail.ru>
2018-07-16 20:37:59 +0300

    my first commit

--- pages/Task2.vue
+++ pages/Task2.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-      Еще более длинный текст с работы набил
+  <div>
+    <div>Blacksquadron</div>
+    <div class="New-area">
+      <div class="blackrectangle"></div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Task2"
-    }
+  export default {
+    name: "Task2"
+  }
 </script>
 
 <style scoped>
+  h2 {
+    top: 20px;
+    text-align: center;
+  }
 
+  .New-area {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    display: flex;
+    align-items: end;
+    justify-content: center;
+  }
+
+  .blackrectangle {
+    background: black;
+    width: 100px;
+    height: 100px;
+  }
 </style>
