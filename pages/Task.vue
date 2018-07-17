82b6b4a129c1dbdb61109836fb8e46f56ba3e0a0
almaz <almaz_73@mail.ru>
2018-07-17 16:35:25 +0300

    my second commit

---
+++ pages/Task.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>Blacksquadron</div>
+    <div class="Orange-area">
+      <div class="orangerectangle"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Task2"
+  }
+</script>
+
+<style scoped>
+  h2 {
+    top: 20px;
+    text-align: center;
+  }
+
+  .Orange-area {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    display: flex;
+    align-items: end;
+    justify-content: center;
+  }
+
+  .orangerectangle {
+    background:orange;
+    width: 100px;
+    height: 100px;
+  }
+</style>
